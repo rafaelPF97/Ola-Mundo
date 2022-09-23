43452a9b1013d0d3f72d881fb7d743eef1991a73
Rafael Passarello Fernandes <rafapf97@gmail.com>
2022-09-23 18:02:06 -0300

    Criei 1 programa em python para github

---
+++ Python-test/ex00.py
@@ -0,0 +1,5 @@
+print('Primeiro teste python em github.')
+a = int(input('Digite um númeiro inteiro: '))
+b = int(input('Dgite outro número inteiro: '))
+soma = a+b
+print(f'A soma entre {a} e {b} é igual a {soma}')
